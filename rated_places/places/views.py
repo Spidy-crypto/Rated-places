42d27313fcf9b6d6b9aa32b3d5ad76bc2069b260
Spidy-crypto <rajkalathiya699@gmail.com>
2021-03-07 21:08:51 +0530

    bug fix

--- rated_places/places/views.py
+++ rated_places/places/views.py
@@ -9,21 +9,21 @@ def index(request):
 
 def category(request):
     city_name = request.GET["city"]
-    category = {'Bars' : "Bars.PNG","Beaches" : "beaches.png","Hospitals" : "hospitals.PNG","Hostels" : "hostels.png", "Hotels" : "hotels.png","Parks" : "parks.png"}
+    category = {'Bars' : "Bars.PNG","Museuem" : "museuem.png","Hospitals" : "hospitals.PNG","Gym" : "gym.png", "Hotels" : "hotels.png","Parks" : "parks.png"}
 
     return render(request,"category.html",{"name" : city_name,"category" : category})
 
 def places(request):
     name = request.GET['name']
-    categories = {'Bars' : "bar","Beaches" : "beaches.png","Hospitals" : "hospital","Hostels" : "hostels.png", "Hotels" : "hotels.png","Parks" : "park"}
+    categories = {'Bars' : "bar","Museuem" : "museuem.png","Hospitals" : "hospital","Gym" : "gym", "Hotels" : "restaurant","Parks" : "park"}
     category = request.GET['category']
 
-    req = requests.get('https://maps.googleapis.com/maps/api/geocode/json?components=country:IN%7Clocality:'+ name + '&key=AIzaSyBD_k7MQjrFHJPUT9MupbUKWOHOsCwl23o')
+    req = requests.get('https://maps.googleapis.com/maps/api/geocode/json?components=country:IN%7Clocality:'+ name + '&key=')
     req = req.json()
     lat =  req['results'][0]['geometry']['location']['lat']
     lang =  req['results'][0]['geometry']['location']['lng']
 
-    r1 = requests.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+ str(lat) + ',' + str(lang) + '&radius=150000&type='+ 'park' + '&key=AIzaSyBD_k7MQjrFHJPUT9MupbUKWOHOsCwl23o')
+    r1 = requests.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+ str(lat) + ',' + str(lang) + '&radius=150000&type='+ categories[category] + '&key=')
 
     r1 = r1.json()
     d = {}
