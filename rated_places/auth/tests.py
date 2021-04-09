e8be8a057a5bbcd862fda6b4bd3299c13c45b81c
Spidy-crypto <rajkalathiya699@gmail.com>
2021-04-09 14:23:58 +0530

    Final commit

--- rated_places/auth/tests.py
+++ rated_places/auth/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.contrib.auth.models import User
+
+class UserTestCase(TestCase):
+
+    def setUp(self):
+        User.objects.create(email = "rajkalathiya123@gmail.com", password = "raj123", id="1")
+
+    def testUserDetail(self):
+        user = User.objects.get(email = "rajkalathiya123@gmail.com")
+        self.assertEqual(user.id,1)
+
+        
--- rated_places/places/tests.py
+++ rated_places/places/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import *
+
+
+class UserTestCase(TestCase):
+
+    def setUp(self):
+        addToFav.objects.create(name="rajkalathiya699@gmail.com", place_name="Pratap Garder", city_name="Surat", place_id = "aknbdj648adk")
+        
+    def testPlaceDetail(self):
+        obj = addToFav.objects.get(name="rajkalathiya699@gmail.com")
+        self.assertEqual(obj.city_name,"Surat")
+
+        
